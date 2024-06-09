53f89e4001226bfaae01ca6f26cf077771e48f2b
Changhao Liu <changhao.liu@lotterywest.wa.gov.au>
2024-06-09 14:37:43 +0800

    improve the image element

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,12 +1,9 @@
 import { useRef, useState } from "react";
 
-type ImageProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement> & {
-  fallbackSrc?: string;
-};
+type ImageProps = React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
 
-export default function Image({ src, alt, style, fallbackSrc, height, ...props }: ImageProps) {
+export default function Image({ src, alt, height}: ImageProps) {
   const imgRef = useRef<HTMLImageElement | null>(null);
-  const [isLoaded, setIsLoaded] = useState(false);
   const [isError, setIsError] = useState(false);
 
   if (isError) {
@@ -15,31 +12,19 @@ export default function Image({ src, alt, style, fallbackSrc, height, ...props }
 
   return (
     <>
-      {!isLoaded && <div>loading</div>}
       <img
-        {...props}
         ref={imgRef}
         src={src}
         loading="lazy"
         style={{
-          opacity: isLoaded ? 1 : 0,
           transition: "opacity 0.25s ease-in-out",
-          height: isLoaded ? height : 0,
+          height: height,
           width: "100%"
         }}
         alt={alt}
-        onError={(e) => {
-          if (fallbackSrc) {
-            if (e.currentTarget.src === fallbackSrc) {
-              setIsError(true);
-              return;
-            }
-            imgRef.current?.setAttribute("src", fallbackSrc);
-            return;
-          }
+        onError={() => {
           setIsError(true);
         }}
-        onLoad={() => setIsLoaded(true)}
       />
     </>
   );
